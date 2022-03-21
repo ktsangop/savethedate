e4c9271d98ac5638da7cabeefad38abbf6e5c5cf
ktsangop <ktsangop@outlook.com>
2022-03-21 21:25:17 +0200

    - Add contact link icons - refactor timer

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+window.onload = function () {
+  window._days = document.querySelector(`.days`);
+  window._hours = document.querySelector(`.hours`);
+  window._mins = document.querySelector(`.minutes`);
+  window._secs = document.querySelector(`.seconds`);
+
+  updateTimer();
+  setInterval('updateTimer()', 1000);
+  document.querySelector(`#linksContainer`).addEventListener('click', () => {
+    if (window.innerWidth < 768) {
+      document.querySelector(`#theHeader`).classList.remove('nav-open');
+      document.querySelector(`.wrapper-menu`).classList.remove('open');
+    }
+  });
+}
+
+function menuToggle() {
+  document.querySelector(`#theHeader`).classList.toggle('nav-open');
+  document.querySelector(`.wrapper-menu`).classList.toggle('open');
+}
+
 function updateTimer() {
   future = Date.parse("Jul 30, 2022 18:00:00");
   now = new Date();
@@ -13,25 +34,8 @@ function updateTimer() {
   m = mins - hours * 60;
   s = secs - mins * 60;
 
-  document.getElementById("timer").innerHTML = `
-    <span>${d}</span> <span>Days</span>
-    <span>${h}</span> <span>Hours</span>
-    <span>${m}</span> <span>Minutes</span>
-    <span>${s}</span> <span>Seconds</span>
-  `;
-}
-
-window.onload = function () {
-  setInterval('updateTimer()', 1000);
-  document.querySelector(`#linksContainer`).addEventListener('click', () => {
-    if (window.innerWidth < 768) {
-      document.querySelector(`#theHeader`).classList.remove('nav-open');
-      document.querySelector(`.wrapper-menu`).classList.remove('open');
-    }
-  });
-}
-
-function menuToggle() {
-  document.querySelector(`#theHeader`).classList.toggle('nav-open');
-  document.querySelector(`.wrapper-menu`).classList.toggle('open');
+  window._days.innerHTML = d;
+  window._hours.innerHTML = h;
+  window._mins.innerHTML = m;
+  window._secs.innerHTML = s;
 }
